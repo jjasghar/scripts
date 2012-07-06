7370a790380866040a57d06ac3d26a04b125fa06
JJ Asghar <jjasghar@utexas.edu>
2012-07-06 16:39:50 -0500

    dumb jj, append not overwrite

--- linux_audit.sh
+++ linux_audit.sh
@@ -3,13 +3,11 @@
 DATE=`date +%F`
 HOSTNAME=`hostname -s`
 
-uname -a > out1.txt
-echo "" > out2.txt
-uptime > out3.txt
-echo "" > out4.txt
-rpm -qa | sort -u > out5.txt
-echo "" > out6.txt
-df -h > out7.txt
-cat out* | tee $HOSTNAME.$DATE.txt
-rm out*.txt
+uname -a >> $HOSTNAME.$DATE.txt
+echo "" >> $HOSTNAME.$DATE.txt
+uptime >> $HOSTNAME.$DATE.txt
+echo "" >> $HOSTNAME.$DATE.txt
+rpm -qa | sort -u >> $HOSTNAME.$DATE.txt
+echo "" >> $HOSTNAME.$DATE.txt
+df -h >> $HOSTNAME.$DATE.txt
 cp $HOSTNAME.$DATE.txt /tmp/
